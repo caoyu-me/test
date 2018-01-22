73b523381759704df0a8261da80ed24690fd892b
caoyu <1481619987@qq.com>
2018-01-22 15:17:41 +0800

    删除敏感信息

--- yuyue/App/Common/Conf/code.php
+++ yuyue/App/Common/Conf/code.php
@@ -14,13 +14,13 @@ return array(
     'EMAIL_PROT'    => '465', // 邮件端口
     'EMAIL_HOST'    => 'smtp.qq.com', // 使用的邮件服务器
     'EMAIL_NAME'     => '1481619987', // 邮箱name
-    'EMAIL_PWD'    => 'gmcrqjuvfsyygccj', // 邮箱的授权密码
+    'EMAIL_PWD'    => '', // 邮箱的授权密码
     'EMAIL_FROM'  => '1481619987@qq.com', // 邮箱地址
     'EMAIL_SUBJECT'=>array('账号激活','验证码'),
     'EMAIL_REGBODY'=>array('http://zx.qydtme.com/register/verification?email=#&code=@','您的验证码是：@。请不要把验证码泄露给其他人。【乾圆大通】'),
     //短信
-    'TEL_USERID' =>  '51368',      // 企业ID
-    'TEL_ACCOUNT' =>  'qianyuandatong',      // 用户账号
-    'TEL_PASSWORD' =>  'qianyuandatong123',      // 用户密码
+    'TEL_USERID' =>  '',      // 企业ID
+    'TEL_ACCOUNT' =>  '',      // 用户账号
+    'TEL_PASSWORD' =>  '',      // 用户密码
     'TEL_REGCONTENT' =>  '您的验证码是：@。请不要把验证码泄露给其他人。【乾圆大通】',      //短信内容 模板
 );
